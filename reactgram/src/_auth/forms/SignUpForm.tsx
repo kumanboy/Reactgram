a0182bd8c1b8267eaea7ce2f6bee4bbb0c31bfab
kumanboy <husandavronov13@gmail.com>
2023-11-15 05:58:43 +0500

    validation(index.ts) updated, appwrite installed, SignUpForm.tsx file Updated

--- reactgram/src/_auth/forms/SignUpForm.tsx
+++ reactgram/src/_auth/forms/SignUpForm.tsx
@@ -2,14 +2,19 @@ import {zodResolver} from "@hookform/resolvers/zod"
 import {Button} from "@/components/ui/button"
 import {useForm} from "react-hook-form";
 import {
-    Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage,
+    Form, FormControl, FormField, FormItem, FormLabel, FormMessage,
 } from "@/components/ui/form"
 import {Input} from "@/components/ui/input"
 import {SignUpValidation} from "@/lib/validation";
 import {z} from "zod";
+import Loader from "@/components/shared/Loader.tsx";
+import {Link} from "react-router-dom";
+import {createUserAccount} from "@/lib/appwrite/api.ts";
 
 
 const SignUpForm = () => {
+
+    const isLoading = false;
 
     const form = useForm<z.infer<typeof SignUpValidation>>({
         resolver: zodResolver(SignUpValidation),
@@ -21,38 +26,91 @@ const SignUpForm = () => {
         },
     })
 
-    // 2. Define a submit handler.
-    function onSubmit(values: z.infer<typeof SignUpValidation>) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        console.log(values)
+    async function onSubmit(values: z.infer<typeof SignUpValidation>){
+        // const newAccount = await createUserAccount(values)
+        // console.log(newAccount)
+        try {
+            const newAccount = await createUserAccount(values)
+            console.log(newAccount)
+        }
+        catch (error) {
+            console.log(error)
+        }
     }
 
     return (
         <Form {...form}>
             <div className="sm:w-420 flex-center flex-col">
-                <img src="/assets/images/logo.png" />
-            </div>
+                <img src="/assets/images/logo.png" alt='logo'/>
+                <h2 className='h3-bold md:h2-bold pt-5 sm:pt-12'>Create New Account</h2>
+                <p className="text-light-3 small-medium md:base-regular mt-2">To use ReactGram,please enter your details</p>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4">
+                    <FormField
+                        control={form.control}
+                        name="name"
+                        render={({field}) => (
+                            <FormItem>
+                                <FormLabel>Name</FormLabel>
+                                <FormControl>
+                                    <Input type='text' className="shad-input" {...field} />
+                                </FormControl>
+                                <FormMessage/>
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="username"
+                        render={({field}) => (
+                            <FormItem>
+                                <FormLabel>Username</FormLabel>
+                                <FormControl>
+                                    <Input type='text' className="shad-input" {...field} />
+                                </FormControl>
+                                <FormMessage/>
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="email"
+                        render={({field}) => (
+                            <FormItem>
+                                <FormLabel>Email</FormLabel>
+                                <FormControl>
+                                    <Input type='email' className="shad-input" {...field} />
+                                </FormControl>
+                                <FormMessage/>
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="password"
+                        render={({field}) => (
+                            <FormItem>
+                                <FormLabel>Password</FormLabel>
+                                <FormControl>
+                                    <Input type='password' className="shad-input" {...field} />
+                                </FormControl>
+                                <FormMessage/>
+                            </FormItem>
+                        )}
+                    />
 
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                <FormField
-                    control={form.control}
-                    name="username"
-                    render={({field}) => (
-                        <FormItem>
-                            <FormLabel>Username</FormLabel>
-                            <FormControl>
-                                <Input placeholder="shadcn" {...field} />
-                            </FormControl>
-                            <FormDescription>
-                                This is your public display name.
-                            </FormDescription>
-                            <FormMessage/>
-                        </FormItem>
-                    )}
-                />
-                <Button type="submit">Submit</Button>
-            </form>
+                    <Button type="submit" className='shad-button_primary'>
+                        {isLoading ? (
+                            <div className="flex-center gap-2">
+                                <Loader/> Loading...
+                            </div>
+                        ):"Sign Up"}
+                    </Button>
+                    <p className='text-small-regular text-light-2 text-center mt-2'>
+                        Already have an account?
+                        <Link to='/sign-in' className='text-primary-500 text-small-semibold ml-1'>Login</Link>
+                    </p>
+                </form>
+            </div>
         </Form>
     )
 }
